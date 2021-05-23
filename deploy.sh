062608ff7e90c25141616280a674225ab21e3d30
arnav <arnav.bhati@cloudev.de>
2021-05-23 13:54:12 +0200

    new changes

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t arnav30/microblog:latest -t arnav30/microblog:$SHA -f ./Dockerfile 
+
+docker push arnav30/microblog:latest
+
+docker push arnav30/microblog:$SHA
+
+kubectl apply -f k8s
+
+
+
