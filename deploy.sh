19195240b09fa35abb9c24548c61c5df5ab3afaf
arnav <arnav.bhati@cloudev.de>
2021-05-23 14:15:20 +0200

    new changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-docker build -t arnav30/microblog:latest -t arnav30/microblog:$SHA -f Dockerfile .
+docker build -t arnav30/microblog:latest -f Dockerfile .
+
+docker build -t arnav30/microblog:$SHA -f Dockerfile .
 
 docker push arnav30/microblog:latest
 
